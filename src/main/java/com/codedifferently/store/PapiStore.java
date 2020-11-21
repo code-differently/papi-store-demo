31c3b1cdef7ffcc5a68bbb9dfbcd58695931fb32
Matt Werth <matthewwerth@gmail.com>
2020-11-21 16:25:47 -0500

    Commented

--- src/main/java/com/codedifferently/store/PapiStore.java
+++ src/main/java/com/codedifferently/store/PapiStore.java
@@ -1,4 +1,4 @@
-//What package we are inside of.
+//What package we are inside of. Family relation of objects.
 package com.codedifferently.store;
 
 //classes we are importing. This way we can use ArrayLists, HashMaps & Map.
@@ -7,21 +7,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 //Create a public/accessable class named Papistore.
-// Add private variables which will be our "State" once we create an object from this class.
+// Add private variables which will be our "State" once we create an object from this class or pass it a value.
 public class PapiStore {
     private boolean isStoreOpen;
     private ArrayList<String> tabs;
     //Create a Map which will hold a name and an amount owed.
+    //Hashmap is a type of Map. Map is the Interface.
     Map<String, Double> owesPapiMoney;
 
     //constructor function that sets up our "State" variables values.
     public PapiStore(){
+        //this allows us to set the initial values - to give our variables values.
+        //THIS refers to the object that will be created, and allows us to create a new instance of the object w/ unique values.
         this.isStoreOpen = false; //initially set store to closed.
         this.tabs = new ArrayList(); // ArrayList of tabs
         this.owesPapiMoney = new HashMap<>(); // Map of who owes, and how much they owe.
     }
 
-    //Second constructor function. This one we can pass in values - we can start the store as open.
+    //Second constructor function. This one we can pass in values - we can start with the isStoreOpen variable set to true or false.
     public PapiStore(Boolean isOpen, ArrayList<String> tabsIn){
         this.isStoreOpen = isOpen;
         this.tabs = tabsIn;
@@ -30,7 +33,6 @@ public class PapiStore {
 
     // This will allow us to close the store.
     public void closeStore(){
-
     }
 
     //this will allow us to open the store, if its already open it wont change anything.
@@ -38,7 +40,7 @@ public class PapiStore {
         this.isStoreOpen = true;
     }
 
-    //tell us if the store is open.
+    //tell us the state of the store - if the store is open or closed.
     public boolean isStoreOpen() {
         return isStoreOpen;
     }
@@ -52,31 +54,38 @@ public class PapiStore {
     }
 
     // A private method for us to check if our name has an overdue balance. Will return true or false.
+    // This method takes in a String name variable.
     private boolean hasAnOverDueBalance(String name){
+        //If this name as a key DOES NOT owe a value return false.
         if(!owesPapiMoney.containsKey(name))
             return false;
         Double money = owesPapiMoney.get(name);
         Boolean owes = false;
+        //if there is a balance, or money owed is greater than 0 return true.
         if(money > 0.0)
             owes = true;
         return owes;
     }
 
     // A private method to tell us if this person has an active tab. Will return true or false.
+    // This method takes in a String name variable.
     private boolean hasAnActiveTab(String name){
+        //Enhanced for loop to go through each tab in tabs ArrayList.
         for (String tab : tabs){
+            //if there is a match to the name - they have an active tab - return true.
             if (tab.equals(name))
                 return true;
         }
+        //If there is no match return false.
         return false;
     }
 
-    //will create an active tab
+    //will create a new active tab by passing a name variable and adding it to our tabs ArrayList.
     public void createActiveTab(String name){
         this.tabs.add(name);
     }
 
-    // print out welcome message.
+    // This will print out a welcome message to the console when we run our program.
     public static void main(String[] args) {
         System.out.println("Welcome to Papi Store, Hola");
     }
--- src/test/java/com/codedifferently/store/PapiStoreTest.java
+++ src/test/java/com/codedifferently/store/PapiStoreTest.java
@@ -3,13 +3,15 @@ package com.codedifferently.store;
 
 // This is importing the Assert Class from the org.junit package or folder
 import org.junit.Assert;
+// This imports the Before class for our @Before from the org.junit package or folder
 import org.junit.Before;
+// This imports our Test class for @Test from the org.junit package or folder
 import org.junit.Test;
 
-// This is a public class called Papi
+// This is a public class, PapiStoreTest set up so that we can test all of the methods in our PapiStore Class.
     public class PapiStoreTest {
 
-        // create a variable to store our object in - outside of the scope of the methods.
+        // creates a variable to store our object in - outside of the scope of the methods so that all of our @Test methods have access.
         // the "ID" of our object.
         PapiStore papiStore;
 
@@ -18,9 +20,12 @@ import org.junit.Test;
         // the method sets up our state by assigning values.
         @Before
         public void setup(){
+            //new object being created for testing.
             papiStore = new PapiStore();
+            //Creating a new active tab for Jamal & Victor.
             papiStore.createActiveTab("Jamal");
             papiStore.createActiveTab("Victor");
+            //Adding a value to our HashMap with the Key or Name and Value as a Double.
             papiStore.owesPapiMoney.put("Jamal", 0.0);
             papiStore.owesPapiMoney.put("Victor", 7.0);
         }
@@ -30,41 +35,41 @@ import org.junit.Test;
         @Test
         public void OpenStoreTest(){
             //Given
-
             papiStore.openStore();
 
             //When
+            //We expect this test to return true when we run the isStoreOpen();
             Boolean expected = true;
             Boolean actual = papiStore.isStoreOpen();
 
             //Then
             //Assert.assertEquals(expected, actual);
+            //Assert that the method returns true when we run it.
             Assert.assertTrue(actual);
         }
 
         @Test
         public void allowToUseTabTestSuccess(){
             // given
-            PapiStore papiStore = new PapiStore();
-
             // when
+            //We expect this test to return true when we run allowToUseTab() and pass in Jamal as an argument;
             Boolean expected = true;
             Boolean actual = papiStore.allowToUseTab("Jamal");
 
             // then
+            //Assert that the method returns true when we run it.
             Assert.assertEquals(expected, actual);
         }
 
         @Test
         public void allowToUseTabTestFail(){
             // given
-            PapiStore papiStore = new PapiStore();
-
             // when
+            //We expect this test to return false when we run allowToUseTab() and pass in Victor as an argument;
             Boolean expected = false;
             Boolean actual = papiStore.allowToUseTab("Victor");
 
-            // then
+            //Assert that the method returns false when we run it.
             Assert.assertEquals(expected, actual);
         }
 }
